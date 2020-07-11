05b95807c186df04b11319b65cfbf57dd9a8ec41
Marcello Maltagliati <marcellomaltagliati@MBP-di-Marcello.attlocal.net>
2020-07-11 16:00:36 -0700

    first commit

---
+++ common/LanguageSwitcher/config.js
@@ -0,0 +1,14 @@
+import us from './flag/us.svg';
+import germany from './flag/germany.svg';
+import spain from './flag/spain.svg';
+import saudi from './flag/saudi.png';
+import chinese from './flag/china.svg';
+import israel from './flag/israel.svg';
+
+export default [
+  {
+    id: 'en',
+    language: 'en',
+    icon: us,
+  },
+];
